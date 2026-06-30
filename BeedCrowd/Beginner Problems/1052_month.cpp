cc0e7efd315af26fa368e16243faf765c5747f11
Mazed-Khan <mazedkhan678@gmail.com>
2026-06-30 22:23:38 +0600

    Solved | BeeCrowd Beginner Problem 1052 |

---
+++ BeedCrowd/Beginner Problems/1052_month.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string months[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+    cout<<months[n-1]<<"\n";
+}
