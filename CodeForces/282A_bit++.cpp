1e34d2e996cead861029752c6d634e98c0e618c8
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-18 22:21:33 +0600

    Solved |Codeforces Problem 282A Bit++|

---
+++ CodeForces/282A_bit++.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>> n;
+
+    int count = 0;
+    while(n--){
+
+        string s;
+        cin>> s;
+
+        if(s == "++X" || s == "X++") count++;
+        else count--;
+
+    }
+    cout << count <<endl;
+    
+
+    return 0;
+}
