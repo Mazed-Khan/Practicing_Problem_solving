877ef32fab612f4b0131b20fd0c69b4e6ce07d80
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-19 11:18:25 +0600

    Solved |Codeforces 236A Boy or Girl |

--- CodeForces/236A_boy_or_girl.cpp
+++ CodeForces/236A_boy_or_girl.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main() {
     
+    string str ;
+    cin >> str ;
     
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    sort(str.begin(), str.end());
+
+    int n = str.length();
+
+    int count = 0;
+    for(int i = 0; i< n; i++ ){
+        
+        if(i >= 1 && str[i] == str[i-1] ){
+            count = count ;
+        }else{
+            count++;
+        }
+    }    
+
+    if(count % 2 == 0){
+        cout<< "CHAT WITH HER!"<<endl;
+    }else{
+        cout<< "IGNORE HIM!"<<endl;
+    }
 
     return 0;
 }
