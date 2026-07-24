2703664eda9d585b87bd99add60bcd11b741620f
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-24 17:58:00 +0600

    Solved |Codeforces Problem - 266B Queue at the School |

---
+++ CodeForces/266B_queue_at_the_school.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    int n,t;
+    cin >> n >> t >> s;
+
+    while (t--)
+    {
+
+        for (int i = 0; i + 1 < n; i++)
+        {
+            if (s[i] == 'B' && s[i + 1] == 'G')
+            {
+                swap(s[i], s[i + 1]);
+                i++; 
+            }
+        }
+    }
+    cout << s;
+
+    return 0;
+}
