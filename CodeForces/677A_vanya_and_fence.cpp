c7e2ffb9285ed33e619176ab7808f0d93cf7dfa5
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-24 09:39:37 +0600

    Solved |Codeforces Problem - 677A Vanya and Fence |

---
+++ CodeForces/677A_vanya_and_fence.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n , h;
+    cin >> n >> h;
+
+    int result = 0;
+
+    for(int i = 0 ; i< n; i++){
+        int a;
+        cin>> a;
+        if( a > h){
+            result +=  2;
+        }else{
+            result += 1;
+        }
+    }
+    cout << result << endl;
+
+
+    return 0;
+}
