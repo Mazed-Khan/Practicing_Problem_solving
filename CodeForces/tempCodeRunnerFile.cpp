82a28d140a74d77e1c529cb878444f9e5463c437
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-27 22:00:14 +0600

    Solved |Codeforces Problem - 61A Ultra Fast Mathematician|

---
+++ CodeForces/tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string s1 , s2 , result ;
+    cin >> s1 >> s2;
+
+    for(int i = 0 ; i < s1.length(); i++){
+        if(s1[i] == '1' && s2[i] == '0' || s1[i] == '0' && s2[i] == '1' ){
+            result[i] = '1';
+        }else{
+            result[i] = '0';
+        }
+    }
+    
+    cout << result << endl;
+
+    return 0;
+}
