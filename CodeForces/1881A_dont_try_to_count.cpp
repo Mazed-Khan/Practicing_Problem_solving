b46f7537d65a42b6657de0201d8aac6ba0219023
Mazed-Khan <mazedkhan678@gmail.com>
2026-07-31 11:29:44 +0600

    Solved |Codeforces Problem - 1881A Don't try to count |

---
+++ CodeForces/1881A_dont_try_to_count.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        string x, s;
+        cin >> x >> s;
+        for(int i = 0; i < 6; i++){
+            if(x.find(s) != string::npos){
+                cout << i << endl;
+                break;
+            }
+            x += x;
+            if(i == 5) cout << -1 << endl;
+        }
+    }
+}
