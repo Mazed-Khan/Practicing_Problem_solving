1738ff03e18f84630c2a6bc12befd010e8b86a16
Mazed-Khan <mazedkhan678@gmail.com>
2026-08-22 22:21:58 +0600

    Solved |BeeCrowd Problem 1071 Sum of Consecutive Odd Numbers I|

---
+++ Beecrowd/Beginner_Problems/1071_sum_of_consecutive_odd_num_i.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    
+    if(x > y){
+        int t = x;
+        x = y;
+        y = t;
+    }
+    
+    int sum = 0;
+    for(int i = x + 1; i < y; i++){
+        if(i % 2 != 0){
+            sum += i;
+        }
+    }
+    
+    cout << sum << endl;
+    
+    return 0;
+}
