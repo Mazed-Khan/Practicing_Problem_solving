4fbe0106ab58f7245dc517201ade496f0b980cb4
Mazed-Khan <mazedkhan678@gmail.com>
2026-08-24 09:45:02 +0600

    Solved |Codeforces Problem - 405A Gravity Flip |

---
+++ Codeforces-800-Rating/405A_gravity_flip.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n ;
+    cin >> n;
+
+    vector<int> vec(n);
+
+    for(int i = 0 ; i< n; i++){
+        cin >> vec[i] ;
+    }
+
+    sort(vec.begin(),vec.end());
+
+    for(int v : vec){
+        cout<< v << " ";
+    }
+    
+
+    return 0;
+}
