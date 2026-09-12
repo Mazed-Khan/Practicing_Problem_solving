93d18d279c6237d473cddf29234669cb95bd0cdb
Mazd-Khan <mazedkhan678@gmail.com>
2026-09-12 18:06:57 +0600

    Solved Codeforces Problem  - 1859A United We stand

---
+++ Codeforces-800-Rating/1859A_united_we_stand.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+
+        int n;
+        cin >> n;
+
+        vector<int> a(n), b, c;
+
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int mx = *max_element(a.begin(), a.end());
+
+        for(int i = 0; i < n; i++) {
+            if(a[i] == mx) {
+                c.push_back(a[i]);
+            } else {
+                b.push_back(a[i]);
+            }
+        }
+
+        if(b.size() == 0 || c.size() == 0) {
+            cout << -1 << endl;
+        }
+        else {
+            cout << b.size() << " " << c.size() << endl;
+
+            for(int i = 0; i < b.size(); i++) {
+                cout << b[i] << " ";
+            }
+            cout << endl;
+
+            for(int i = 0; i < c.size(); i++) {
+                cout << c[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
