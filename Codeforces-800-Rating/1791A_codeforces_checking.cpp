bcc2bf54f139c1b220973639b307a574497b89b7
Mazd-Khan <mazedkhan678@gmail.com>
2026-09-15 22:49:42 +0600

    Solved Codeforces Problem  - 1791A Codeforces Checking

---
+++ Codeforces-800-Rating/1791A_codeforces_checking.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    string s = "codeforces";
+
+    while (t--) {
+        char c;
+        cin >> c;
+
+        if (s.find(c) != string::npos)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
